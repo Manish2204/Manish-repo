d0b1cbff9c246d87f00fdf6f59d1f29f531ef3e3
Manish <manish05609@gmail.com>
2024-06-05 12:53:19 +0530

    add master branch

--- routing-practice-nextjs/src/app/course/[...c]/page.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-function C({ params }) {
-  console.log(params);
-  return <div>{params.c}</div>;
-}
-
-export default C;
--- routing-practice-nextjs/src/app/course/error.jsx
+++
@@ -1,7 +0,0 @@
-// import React from "react";
-"use client";
-const error = () => {
-  return <div>error</div>;
-};
-
-export default error;
